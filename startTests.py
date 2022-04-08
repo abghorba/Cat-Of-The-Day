4b508e2f72ecc4cffddfb07b2b554ed93fefd1f5
Andrew Ghorbani <andrewbghorbani@gmail.com>
2022-04-07 18:20:15 -0700

    Added command line arguments for testing purposes

--- startTests.py
+++ startTests.py
@@ -1,15 +1,21 @@
 import logging
 import os
 import pytest
+import sys
 
 from datetime import datetime
 
+if len(sys.argv) != 2:
+    raise Exception("Correct usage: python startTests.py <test_name>")
 
-log_filename = datetime.now().strftime("%d%m%Y%H%M%S") + "-unit_tests"
+args = sys.argv[1:]
+test_to_run = args[0]
+
+log_filename = datetime.now().strftime("%d%m%Y%H%M%S") + f"-{test_to_run}"
 log_filepath = os.getcwd() + f"/logs/{log_filename}.log"
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(module)s.py - %(funcName)s - [%(levelname)s] %(message)s",
                     handlers=[logging.FileHandler(log_filepath), logging.StreamHandler()])
 
-pytest.main([os.getcwd() + "/tests/unit_tests.py", "-v"])
+pytest.main([os.getcwd() + f"/tests/{test_to_run}.py", "-v"])
