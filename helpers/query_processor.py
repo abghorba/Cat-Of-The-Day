3a0605e37275ac7cb76e5548f77f474069434859
Andrew Ghorbani <andrewbghorbani@gmail.com>
2022-07-13 00:36:34 -0700

    working on algorithms for finding keywords/key phrases in text

--- helpers/query_processor.py
+++ helpers/query_processor.py
@@ -1,15 +1,17 @@
-import random
 import re
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 
+
+STOP_WORDS = set(stopwords.words('english'))
 
 # Want to use Natural Language Processing to understand user requests
 class QueryProcessor():
-    """Class to handle text processing."""
+    """Class to handle query processing."""
 
-    def clean_text(self, text):
+    def _clean_text(self, text):
         """
         Clean the text to be usable. Strips leading/trailing
         whitespace, strips punctuation, and converts string to
@@ -19,37 +21,15 @@ class QueryProcessor():
         :return: Cleaned text as a string
         """
         if not text:
-            return None
+            return ""
 
         clean_text = text.strip()
-        clean_text = re.sub(r"[^\w\s]", "", clean_text)
+        clean_text = re.sub(r"[^\w\s\']", "", clean_text)g
         clean_text = clean_text.lower()
 
         return clean_text
 
-    def find_keywords_in_text(self, keywords, text):
-        """
-        Given a dictionary of keywords, finds if any keyword
-        exists in a text.
-
-        :param keywords: Dictionary containing the keywords to search for
-        :param text: The text to search for keywords in
-        :return: The first keyword found in text
-        """
-
-        # If text has multiple keywords, find the first keyword randomly
-        keywords_list = list(keywords.keys())
-        random.shuffle(keywords_list)
-
-        for key in keywords_list:
-
-            if key in text:
-
-                return key
-
-        return None
-
-    def preprocess_text(self, text):
+    def _preprocess_text(self, text):
         """
         Preprocesses text by stripping whitespace, removing
         punctuation, changing all characters to lowercase,
@@ -59,20 +39,75 @@ class QueryProcessor():
         :return: A list of all preprocessed words
         """
 
-        text = text.strip()
-        text = re.sub(r"[^\w\s]", "", text)
-        text = text.lower()
-        text_tokens = text.split(" ")
+        cleaned_text = self._clean_text(text)
+        text_tokens = word_tokenize(cleaned_text)
 
-        stop_words = set(stopwords.words("english"))
         lemmatizer = WordNetLemmatizer()
         processed_text = []
 
         for word in text_tokens:
 
-            if word not in stop_words:
-
+            if word not in STOP_WORDS:
                 lemma = lemmatizer.lemmatize(word)
                 processed_text.append(lemma)
 
         return processed_text
+
+    def find_keywords_in_text(self, keywords, text):
+        """
+        Given a set of keywords, returns a dictionary with
+        the found keywords and the frequency of the keyword in
+        text.
+
+        :param keywords: Set containing the keywords to search for
+        :param text: The text to search for keywords in
+        :return: The first keyword found in text
+        """
+
+        if not keywords or not isinstance(keywords, set):
+            raise TypeError("keywords param must be a set")
+
+        if not text or not isinstance(text, str):
+            raise TypeError("text param must be a str")
+
+        preprocessed_text = self._preprocess_text(text)
+        found_keywords = {}
+
+        for word in preprocessed_text:
+
+            if word in keywords:
+                found_keywords[word] =  found_keywords.get(word, 0) + 1
+
+        return found_keywords
+
+    def find_key_phrases_in_text(self, key_phrases, text):
+        """
+        Given a set of key phrases, returns a dictionary with
+        the found key phrases and the frequency of the key phrase in
+        text.
+
+        A key phrase is defined as any string.
+
+        :param key_phrases: Set containing the key_phrases to search for
+        :param text: The text to search for keywords in
+        :return: The first keyword found in text
+        """
+
+        if not key_phrases or not isinstance(key_phrases, set):
+            raise TypeError("keywords param must be a set")
+
+        if not text or not isinstance(text, str):
+            raise TypeError("text param must be a str")
+
+        preprocessed_text = " ".join(self._preprocess_text(text))
+        print(preprocessed_text)
+
+        found_key_phrases = {}
+
+        for key_phrase in key_phrases:
+
+            if key_phrase in preprocessed_text:
+                found_key_phrases[key_phrase] = preprocessed_text.count(key_phrase)
+
+        return found_key_phrases
+
---
+++ tests/test_query_processor.py
@@ -0,0 +1,156 @@
+import pytest
+
+from helpers.query_processor import QueryProcessor
+from nltk.corpus import stopwords
+
+
+STOP_WORDS = set(stopwords.words('english'))
+
+query_processor = QueryProcessor()
+
+
+@pytest.mark.parametrize(
+    "text,expected_cleaned_text",
+    [
+        ("What a marvelous day it is today!", "what a marvelous day it is today"),
+        ("There isn't a place I'd rather be.", "there isn't a place i'd rather be"),
+        (" why That There is SIMPLY MAGNIFICENT, I must say... ", "why that there is simply magnificent i must say"),
+        ("", ""),
+        (" ", ""),
+        ("!@#$%^&*()-+=][;\".,`~}{", ""),
+        (None, "")
+    ]
+)
+def test_clean_test(text, expected_cleaned_text):
+    """
+    Tests that clean_text() works as intended:
+    
+    'Strips leading/trailing whitespace, strips punctuation, 
+    and converts string to lowercase.' 
+    
+    :param text: str 
+    :param expected_clean_text: str 
+    """
+    
+    cleaned_text = query_processor._clean_text(text)
+    assert cleaned_text == expected_cleaned_text
+
+
+@pytest.mark.parametrize(
+    "text,expected_preprocessed_text",
+    [
+        ("", []),
+        ("", []),
+        ("", []),
+        ("", []),
+        ("", []),
+    ]
+)
+def test_preprocess_text(text, expected_preprocessed_text):
+    """
+    Test that preprocess_text() works as intended:
+
+    'Preprocesses text by stripping whitespace, removing
+    punctuation, changing all characters to lowercase,
+    tokenizing by whitespaces, and lemmatizing each word.'
+
+    :param text: str
+    :param expected_preprocessed_text: list
+    """
+
+    preprossed_text = query_processor._preprocess_text(text)
+    assert preprossed_text == expected_preprocessed_text
+
+
+@pytest.mark.parametrize(
+    "keywords,text,expected_keywords_found",
+    [
+        (
+            {"crystal", "blue", "persuasion"},
+            "Crystal blue Persuasion, hmm, hmm It's a new vibration CrYsTal blue persuasion Crystal Blue persuasion",
+            {"crystal": 3, "blue": 3, "persuasion": 3}
+        ),
+        (
+            {"lorem", "magna", "nonsense"},
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            {"lorem": 1, "magna": 1}
+        ),
+        (
+            {"man", "silver", "heaven"},
+            "There's a lady who's sure all that glitters is gold. And she's buying a stairway to heaven.",
+            {"heaven": 1}
+        ),
+        (
+            {"taking", "everything"},
+            "Takin' everythin' in my stride",
+            {}
+        ),
+        (
+            STOP_WORDS,
+            " ".join(stopwords.words('english')),
+            {}
+        )
+    ]
+)
+def test_find_keywords_in_text(keywords, text, expected_keywords_found):
+    """
+    Tests that find_keywords_in_text() works as intended:
+
+    'Given a set of keywords, returns a dictionary with
+    the found keywords and the frequency of the keyword in
+    text.'
+
+    :param keywords:
+    :param text:
+    :param expected_keywords_found:
+    """
+    
+    keywords_found = query_processor.find_keywords_in_text(keywords, text)
+    assert keywords_found == expected_keywords_found
+
+
+@pytest.mark.parametrize(
+    "key_phrases,text,expected_key_phrases_found",
+    [
+        (
+            {"crystal blue persuasion"},
+            "Crystal blue Persuasion, hmm, hmm It's a new vibration CrYsTal blue persuasion Crystal Blue persuasion",
+            {"crystal blue persuasion": 3}
+        ),
+        (
+            {"lorem ipsum dolor", "magna aliqua", "nonsensical text"},
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            {"lorem ipsum dolor": 1, "magna aliqua": 1}
+        ),
+        (
+            {"man silver", "heaven"},
+            "There's a lady who's sure all that glitters is gold. And she's buying a stairway to heaven.",
+            {"heaven": 1}
+        ),
+        (
+            {"taking everything"},
+            "Takin' everythin' in my stride",
+            {}
+        ),
+        (
+            STOP_WORDS,
+            " ".join(stopwords.words('english')),
+            {}
+        )
+    ]
+)
+def test_find_key_phrases_in_text(key_phrases, text, expected_key_phrases_found):
+    """
+    Tests that find_key_phrases_in_text() works as intended:
+
+    'Given a set of key phrases, returns a dictionary with
+    the found key phrases and the frequency of the keyphrases in
+    text.'
+
+    :param key_phrases:
+    :param text:
+    :param expected_key_phrases_found:
+    """
+    
+    key_phrases_found = query_processor.find_key_phrases_in_text(key_phrases, text)
+    assert key_phrases_found == expected_key_phrases_found
